e6e81868e7212d8ad3ecd00528de3092f08bdf45
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2020-10-13 18:17:19 +0100

    Update check.js

--- Day 10 - Hold Shift and Check Checkboxes/check.js
+++ Day 10 - Hold Shift and Check Checkboxes/check.js
@@ -1,27 +1,28 @@
-const checkboxes = document.querySelectorAll('.container input[type="checkbox"]');
+const checkboxes = document.querySelectorAll('.wrapper input[type="checkbox"]');
 
 let lastChecked;
 
 function handleCheck(e) {
-  // Check if they had the shift key down
-  // AND check that they are checking it
-  let inBetween = false;
-  if (e.shiftKey && this.checked) {
-    // go ahead and do what we please
-    // loop over every single checkbox
-    checkboxes.forEach(checkbox => {
-      console.log(checkbox);
-      if (checkbox === this || checkbox === lastChecked) {
-        inBetween = !inBetween;
-      }
 
-      if (inBetween) {
-        checkbox.checked = true;
-      }
-    });
-  }
+    let inBetween = false;
 
-  lastChecked = this;
+    if (e.shiftKey && this.checked) {
+
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('lets hear it');
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+
+
+    lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
