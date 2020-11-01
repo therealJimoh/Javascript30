bfb6253adb30aee6aee455d1a4f8b7b7d69f69c4
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2020-11-01 04:01:29 +0100

    Add files via upload

---
+++ Day 30 - Whack a Mole Game/game.js
@@ -0,0 +1,49 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false
+let score = 0
+
+function randomTime(min, max) {
+    return Math.random(Math.random() * (max + min) + min);
+}
+
+function randomHoles(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        console.log('same one buddy');
+        return randomHoles(holes);
+    }
+
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 2000);
+    const hole = randomHoles(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time)
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 2000)
+}
+
+function bonk(e) {
+    if (!e.isTrusted) return;
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
